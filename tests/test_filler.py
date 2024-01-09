b172fc2232bab9653fb43356f605aa1118bbd178
phzwart <PHZwart@lbl.gov>
2024-01-08 16:54:11 -0800

    Another test for filler functions

--- tests/test_filler.py
+++ tests/test_filler.py
@@ -377,11 +377,83 @@ def test_both():
 """
     assert result == expected_result
 
+def test_matrix():
+    N = 32
+    volume = np.zeros((N,N,N))
+    class_map = np.zeros((N, N, N)).astype(np.int8)
+    instance_map = np.zeros((N, N, N)).astype(np.int8)
+
+    xyz = (N/2.0, N/4.0, N/4.0)
+    fillers.fill_ellipsoid(center = xyz,
+                           major_axis=N//6,
+                           minor_axis=N//6,
+                           volume = volume,
+                           class_map = class_map,
+                           instance_map = instance_map,
+                           instance_label = 1,
+                           class_label = 3)
+
+    xyz = (N/2.0, N - N/4.0, N - N/4.0)
+
+    fillers.fill_sphere(center = xyz,
+                        radius = N//5,
+                        volume = volume,
+                        class_map = class_map,
+                        instance_map = instance_map,
+                        instance_label = 2,
+                        class_label = 2)
+
+    xyz = (N/2.0, N/2.0, N/2.0)
+    fillers.matrix(center = xyz,
+                   radius = N//1.75,
+                   volume = volume,
+                   class_map = class_map,
+                   instance_map = instance_map,
+                   instance_label = 0,
+                   class_label = 1,
+                   sel_label=2)
+
+    result = fillers.array_to_ascii_art(class_map[N//2,...])
+    expected_result = """........:::::::::::::::::.......
+.......:::::::::::::::::::......
+.....:::::::::::::::::::::::....
+....:::::::::::::::::::::::::...
+...:::%%%%%:::::::::::::::::::..
+..:::%%%%%%%%::::::::::::::::::.
+..::%%%%%%%%%::::::::::::::::::.
+.:::%%%%%%%%%:::::::::::::::::::
+::::%%%%%%%%%:::::::::::::::::::
+::::%%%%%%%%%:::::::::::::::::::
+::::%%%%%%%%%:::::::::::::::::::
+::::%%%%%%%%::::::::::::::::::::
+::::::%%%%%:::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::
+:::::::::::::::::::::+++++++::::
+::::::::::::::::::::+++++++++:::
+:::::::::::::::::::+++++++++++::
+:::::::::::::::::::+++++++++++::
+:::::::::::::::::::+++++++++++::
+:::::::::::::::::::+++++++++++::
+.::::::::::::::::::+++++++++++::
+..:::::::::::::::::+++++++++++:.
+..:::::::::::::::::+++++++++++:.
+...:::::::::::::::::+++++++++:..
+....:::::::::::::::::+++++++:...
+.....:::::::::::::::::::::::....
+.......:::::::::::::::::::......
+"""
+    assert result == expected_result
 
 if __name__ =="__main__":
     test_sphere()
     test_ellipsoid()
     test_both()
+    test_matrix()
 
 
 
