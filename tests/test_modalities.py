d68399432865b55b324490024952a31e3dcd9826
phzwart <PHZwart@lbl.gov>
2024-01-09 19:12:39 -0800

    minor updates

--- tests/test_modalities.py
+++ tests/test_modalities.py
@@ -28,17 +28,24 @@ def test_build_mode():
     modality_map = modalities.compute_weighted_map(new_class_map,class_actions)
     slicer = cutter.schaaf(modality_map)
     plakje = slicer.plakje((1,0,0), (16,16,16), 32, 1, modality_map)
+    assert plakje.shape[0] == 1
 
     cs1 = utils.compute_checksum(utils.array_to_ascii_art(plakje[0]))
     ref_cs1 = "71511f711ef2d757493dfee1e350f22327bc3826da8f98c351d8ef22d95b1a0a"
     assert cs1 == ref_cs1
 
-    #import matplotlib.pyplot as plt
-    #plt.imshow(plakje[0])
-    #plt.show()
-    #input()
+    class_actions = np.array([[0,0],[0,1],[1.0,0.1],[4.0,0.5]])
+    modality_map = modalities.compute_weighted_map(new_class_map,class_actions)
+    slicer = cutter.schaaf(modality_map)
+    plakje = slicer.plakje((1,0,0), (16,16,16), 32, 1, modality_map)
+    assert plakje.shape[0] == 2
 
-
+    cs1 = utils.compute_checksum(utils.array_to_ascii_art(plakje[0]))
+    cs2 = utils.compute_checksum(utils.array_to_ascii_art(plakje[1]))
+    ref_cs1 = "71511f711ef2d757493dfee1e350f22327bc3826da8f98c351d8ef22d95b1a0a"
+    ref_cs2 = "a205895d1d6e72ec9754864f0dd94ea759264fb4bb7d0d4204fa95da6b859a85"
+    assert cs1 == ref_cs1
+    assert cs2 == ref_cs2
 
 
 
