6f4b2e1ce4a2cde749603810b7f93f3abccd862e
phzwart <PHZwart@lbl.gov>
2024-01-10 15:56:22 -0800

    make some imports easier

--- mm3dtestdata/__init__.py
+++ mm3dtestdata/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Petrus H. Zwart"""
 __email__ = 'PHZwart@lbl.gov'
 __version__ = '0.1.0'
+
+from .noise import noise
+from .cutter import schaaf
+from .builder import balls_and_eggs
+from .modalities import compute_weighted_map
+from .blur import blur_it
